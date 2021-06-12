d3939aee6b19f0c325f5d6ef83da7f31d0926011
Kyoko <kyokoconnect@gmail.com>
2021-06-12 00:49:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="text-center">
+        <h1>Shopping List Generator</h1>
+        <h3>Plan out before you go out! Check in items after each shopping!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
